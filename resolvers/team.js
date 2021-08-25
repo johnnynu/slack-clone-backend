f6e669f54f4f87d2300eac808f4ab2722af2627f
johnnynu <johnnynu98@gmail.com>
2021-08-24 22:29:33 -0700

    implemented authentication check

--- resolvers/team.js
+++ resolvers/team.js
@@ -1,21 +1,24 @@
 import formatErrors from '../formatErrors';
+import requiresAuth from '../permissions';
 
 export default {
   Mutation: {
-    createTeam: async (parent, args, { models, user }) => {
-      console.log(user);
-      try {
-        await models.Team.create({ ...args, owner: user.id });
-        return {
-          success: true,
-        };
-      } catch (err) {
-        console.log(err);
-        return {
-          success: false,
-          errors: formatErrors(err),
-        };
+    createTeam: requiresAuth.createResolver(
+      async (parent, args, { models, user }) => {
+        console.log(user);
+        try {
+          await models.Team.create({ ...args, owner: user.id });
+          return {
+            success: true,
+          };
+        } catch (err) {
+          console.log(err);
+          return {
+            success: false,
+            errors: formatErrors(err),
+          };
+        }
       }
-    },
+    ),
   },
 };
